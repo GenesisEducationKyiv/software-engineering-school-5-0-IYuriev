8fbf1be441fd85b063928ec80032a7e57ed36a7d
IYuriev <ilya.yuriev.s@gmail.com>
2025-05-19 19:31:09 +0300

    feat: add front-end logic for weather updates and subscription handling

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,18 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.setGlobalPrefix('api');
   app.useLogger(app.get(WinstonLogger));
+
+  app.enableCors({
+    origin: [
+      'http://localhost:5500',
+      'http://127.0.0.1:5500',
+      'http://localhost:3000',
+    ],
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: '*',
+  });
+
   await app.listen(Number(process.env.PORT));
 }
 
