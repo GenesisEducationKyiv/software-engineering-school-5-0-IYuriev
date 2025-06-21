3141ad2ae126f0dc7c253105e8e505e727c6971a
IYuriev <ilya.yuriev.s@gmail.com>
2025-06-21 20:17:04 +0300

    refactor: add OpenWeatherData type; update imports

--- src/weather/interfaces/weather-service.interface.ts
+++ src/weather/interfaces/weather-service.interface.ts
@@ -2,7 +2,10 @@ import { WeatherResponse } from '../../constants/types/weather';
 
 export interface WeatherClient {
   getWeather(city: string): Promise<WeatherResponse>;
-  validateCity?(city: string): Promise<string>;
+}
+
+export interface CityValidatable {
+  validateCity(city: string): Promise<string>;
 }
 
 export const WeatherClientToken = Symbol('WeatherClient');
